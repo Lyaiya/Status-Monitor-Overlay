82ddbb2ce8d6b11ce2e7e94aa667dfaba9403e2f
Damien Zhao <zdm65477730@126.com>
2024-07-04 22:11:24 +0800

    add multi-lang suport
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- source/modes/Full.hpp
+++ source/modes/Full.hpp
@@ -29,8 +29,10 @@ private:
 	FullSettings settings;
 	uint64_t systemtickfrequency_impl = systemtickfrequency;
 public:
-    FullOverlay() { 
-		GetConfigSettings(&settings);
+    FullOverlay() {
+		tsl::hlp::doWithSDCardHandle([this] {
+			GetConfigSettings(&settings);
+		});
 		mutexInit(&mutex_BatteryChecker);
 		mutexInit(&mutex_Misc);
 		tsl::hlp::requestForeground(false);
@@ -40,24 +42,23 @@ public:
 			tsl::gfx::Renderer::getRenderer().setLayerPos(1248, 0);
 		}
 		deactivateOriginalFooter = true;
-        StartThreads();
+		StartThreads();
 	}
 	~FullOverlay() {
 		CloseThreads();
 		FullMode = true;
 		tsl::hlp::requestForeground(true);
-		alphabackground = 0xD;
+		IsFrameBackground = true;
 		if (settings.setPosRight) {
 			tsl::gfx::Renderer::getRenderer().setLayerPos(0, 0);
 		}
 		deactivateOriginalFooter = false;
 	}
 
-    virtual tsl::elm::Element* createUI() override {
-		rootFrame = new tsl::elm::OverlayFrame("Status Monitor", APP_VERSION);
+	virtual tsl::elm::Element* createUI() override {
+		rootFrame = new tsl::elm::OverlayFrame("PluginName"_tr, APP_VERSION);
 
 		auto Status = new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h) {
-			
 			//Print strings
 			///CPU
 			if (R_SUCCEEDED(clkrstCheck) || R_SUCCEEDED(pcvCheck)) {
@@ -66,15 +67,15 @@ public:
 				if (realCPU_Hz && settings.showRealFreqs) {
 					height_offset = 162;
 				}
-				renderer->drawString("CPU Usage:", false, COMMON_MARGIN, 120, 20, renderer->a(0xFFFF));
-				auto dimensions = renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
+				renderer->drawString("CPUUsageFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, 120, 20, renderer->a(0xFFFF));
+				auto dimensions = renderer->drawString("CPUTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
 				uint32_t offset = COMMON_MARGIN + dimensions.first;
 				if (settings.showTargetFreqs) {
-					renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
+					renderer->drawString("CPUTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
 					renderer->drawString(CPU_Hz_c, false, offset, height_offset, 15, renderer->a(0xFFFF));
 				}
 				if (realCPU_Hz && settings.showRealFreqs) {
-					renderer->drawString("Real Frequency:", false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
+					renderer->drawString("CPURealFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
 					renderer->drawString(RealCPU_Hz_c, false, offset, height_offset - 15, 15, renderer->a(0xFFFF));
 					if (settings.showDeltas && settings.showTargetFreqs) {
 						renderer->drawString(DeltaCPU_c, false, COMMON_MARGIN + 230, height_offset - 7, 15, renderer->a(0xFFFF));
@@ -88,26 +89,26 @@ public:
 				}
 				renderer->drawString(CPU_compressed_c, false, COMMON_MARGIN, height_offset + 30, 15, renderer->a(0xFFFF));
 			}
-			
+
 			///GPU
 			if (R_SUCCEEDED(clkrstCheck) || R_SUCCEEDED(pcvCheck) || R_SUCCEEDED(nvCheck)) {
-				
+
 				uint32_t height_offset = 320;
 				if (realGPU_Hz && settings.showRealFreqs) {
 					height_offset = 327;
 				}
 
-				renderer->drawString("GPU Usage:", false, COMMON_MARGIN, 285, 20, renderer->a(0xFFFF));
+				renderer->drawString("GPUUsageFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, 285, 20, renderer->a(0xFFFF));
 				if (R_SUCCEEDED(clkrstCheck) || R_SUCCEEDED(pcvCheck)) {
-					auto dimensions = renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
+					auto dimensions = renderer->drawString("GPUTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
 					uint32_t offset = COMMON_MARGIN + dimensions.first;
-					if (settings.showTargetFreqs) { 
-						renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
+					if (settings.showTargetFreqs) {
+						renderer->drawString("GPUTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
 						renderer->drawString(GPU_Hz_c, false, offset, height_offset, 15, renderer->a(0xFFFF));
 
 					}
 					if (realCPU_Hz && settings.showRealFreqs) {
-						renderer->drawString("Real Frequency: ", false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
+						renderer->drawString("GPURealFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
 						renderer->drawString(RealGPU_Hz_c, false, offset, height_offset - 15, 15, renderer->a(0xFFFF));
 						if (settings.showDeltas && settings.showTargetFreqs) {
 							renderer->drawString(DeltaGPU_c, false, COMMON_MARGIN + 230, height_offset - 7, 15, renderer->a(0xFFFF));
@@ -123,27 +124,26 @@ public:
 				if (R_SUCCEEDED(nvCheck)) {
 					renderer->drawString(GPU_Load_c, false, COMMON_MARGIN, height_offset + 15, 15, renderer->a(0xFFFF));
 				}
-				
 			}
-			
+
 			///RAM
 			if (R_SUCCEEDED(clkrstCheck) || R_SUCCEEDED(pcvCheck) || R_SUCCEEDED(Hinted)) {
-				
+
 				uint32_t height_offset = 410;
 				if (realRAM_Hz && settings.showRealFreqs) {
 					height_offset += 7;
 				}
 
-				renderer->drawString("RAM Usage:", false, COMMON_MARGIN, 375, 20, renderer->a(0xFFFF));
+				renderer->drawString("RAMUsageFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, 375, 20, renderer->a(0xFFFF));
 				if (R_SUCCEEDED(clkrstCheck) || R_SUCCEEDED(pcvCheck)) {
-					auto dimensions = renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
+					auto dimensions = renderer->drawString("RAMTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0x0000));
 					uint32_t offset = COMMON_MARGIN + dimensions.first;
 					if (settings.showTargetFreqs) {
-						renderer->drawString("Target Frequency: ", false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
+						renderer->drawString("RAMTargetFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset, 15, renderer->a(0xFFFF));
 						renderer->drawString(RAM_Hz_c, false, offset, height_offset, 15, renderer->a(0xFFFF));
 					}
 					if (realRAM_Hz && settings.showRealFreqs) {
-						renderer->drawString("Real Frequency:", false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
+						renderer->drawString("RAMRealFrequencyFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset - 15, 15, renderer->a(0xFFFF));
 						renderer->drawString(RealRAM_Hz_c, false, offset, height_offset - 15, 15, renderer->a(0xFFFF));
 						if (settings.showDeltas && settings.showTargetFreqs) {
 							renderer->drawString(DeltaRAM_c, false, COMMON_MARGIN + 230, height_offset - 7, 15, renderer->a(0xFFFF));
@@ -160,41 +160,40 @@ public:
 					}
 				}
 				if (R_SUCCEEDED(Hinted)) {
-					auto dimensions = renderer->drawString("Total: \nApplication: \nApplet: \nSystem: \nSystem Unsafe: ", false, 0, height_offset + 40, 15, renderer->a(0x0000));
-					renderer->drawString("Total: \nApplication: \nApplet: \nSystem: \nSystem Unsafe: ", false, COMMON_MARGIN, height_offset + 40, 15, renderer->a(0xFFFF));
+					auto dimensions = renderer->drawString("RAMTotalUsageFullOverlayCustomDrawerText"_tr.c_str(), false, 0, height_offset + 40, 15, renderer->a(0x0000));
+					renderer->drawString("RAMTotalUsageFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, height_offset + 40, 15, renderer->a(0xFFFF));
 					renderer->drawString(RAM_var_compressed_c, false, COMMON_MARGIN + dimensions.first, height_offset + 40, 15, renderer->a(0xFFFF));
 				}
 			}
-			
+
 			///Thermal
 			if (R_SUCCEEDED(tsCheck) || R_SUCCEEDED(tcCheck) || R_SUCCEEDED(fanCheck)) {
-				renderer->drawString("Board:", false, 20, 550, 20, renderer->a(0xFFFF));
+				renderer->drawString("ThermalUsageFullOverlayCustomDrawerText"_tr.c_str(), false, 20, 550, 20, renderer->a(0xFFFF));
 				if (R_SUCCEEDED(tsCheck)) renderer->drawString(BatteryDraw_c, false, COMMON_MARGIN, 575, 15, renderer->a(0xFFFF));
 				if (R_SUCCEEDED(tsCheck)) {
-					auto dimensions1 = renderer->drawString("Temperatures: ", false, 0, 590, 15, renderer->a(0x0000));
-					auto dimensions2 = renderer->drawString("SoC \nPCB \nSkin ", false, 0, 590, 15, renderer->a(0x0000));
-					renderer->drawString("Temperatures:", false, COMMON_MARGIN, 590, 15, renderer->a(0xFFFF));
-					renderer->drawString("SoC\nPCB\nSkin", false, COMMON_MARGIN + dimensions1.first, 590, 15, renderer->a(0xFFFF));
+					auto dimensions1 = renderer->drawString("ThermalTemperaturesDimensions1FullOverlayCustomDrawerText"_tr.c_str(), false, 0, 590, 15, renderer->a(0x0000));
+					auto dimensions2 = renderer->drawString("ThermalTemperaturesDimensions2FullOverlayCustomDrawerText"_tr.c_str(), false, 0, 590, 15, renderer->a(0x0000));
+					renderer->drawString("ThermalTemperaturesFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN, 590, 15, renderer->a(0xFFFF));
+					renderer->drawString("ThermalTemperaturesItemsFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN + dimensions1.first, 590, 15, renderer->a(0xFFFF));
 					renderer->drawString(SoCPCB_temperature_c, false, COMMON_MARGIN + dimensions1.first + dimensions2.first, 590, 15, renderer->a(0xFFFF));
 				}
 				if (R_SUCCEEDED(fanCheck)) renderer->drawString(Rotation_SpeedLevel_c, false, COMMON_MARGIN, 635, 15, renderer->a(0xFFFF));
 			}
-			
+
 			///FPS
 			if (GameRunning) {
 				uint32_t width_offset = 230;
-				auto dimensions = renderer->drawString("PFPS: \nFPS:", false, COMMON_MARGIN + width_offset, 120, 20, renderer->a(0xFFFF));
+				auto dimensions = renderer->drawString("FPSTitleFullOverlayCustomDrawerText"_tr.c_str(), false, COMMON_MARGIN + width_offset, 120, 20, renderer->a(0xFFFF));
 				uint32_t offset = COMMON_MARGIN + width_offset + dimensions.first;
 				renderer->drawString(FPS_var_compressed_c, false, offset, 120, 20, renderer->a(0xFFFF));
 			}
-			
+
 			std::string formattedKeyCombo = keyCombo;
 			formatButtonCombination(formattedKeyCombo);
-			
-			std::string message = "Hold " + formattedKeyCombo + " to Exit";
-			
+
+			std::string message = "HoldRefreshRateHitFullOverlayCustomDrawerText"_tr.c_str() + formattedKeyCombo + "ExitRefreshRateHitFullOverlayCustomDrawerText"_tr.c_str();
+
 			renderer->drawString(message.c_str(), false, COMMON_MARGIN, 693, 23, renderer->a(0xFFFF));
-			
 		});
 
 		rootFrame->setContent(Status);
@@ -206,17 +205,17 @@ public:
 		//Make stuff ready to print
 		///CPU
 		if (idletick0 > systemtickfrequency_impl)
-			strcpy(CPU_Usage0, "Core #0: 0.00%");
-		else snprintf(CPU_Usage0, sizeof CPU_Usage0, "Core #0: %.2f%%", (1.d - ((double)idletick0 / systemtickfrequency_impl)) * 100);
+			strcpy(CPU_Usage0, "UpdateDefaultCPUCore0FullOverlayCustomDrawerText"_tr.c_str());
+		else snprintf(CPU_Usage0, sizeof CPU_Usage0, "UpdateCPUCore0FullOverlayCustomDrawerText"_tr.c_str(), (1.d - ((double)idletick0 / systemtickfrequency_impl)) * 100);
 		if (idletick1 > systemtickfrequency_impl)
-			strcpy(CPU_Usage1, "Core #1: 0.00%");
-		else snprintf(CPU_Usage1, sizeof CPU_Usage1, "Core #1: %.2f%%", (1.d - ((double)idletick1 / systemtickfrequency_impl)) * 100);
+			strcpy(CPU_Usage1, "UpdateDefaultCPUCore1FullOverlayCustomDrawerText"_tr.c_str());
+		else snprintf(CPU_Usage1, sizeof CPU_Usage1, "UpdateCPUCore1FullOverlayCustomDrawerText"_tr.c_str(), (1.d - ((double)idletick1 / systemtickfrequency_impl)) * 100);
 		if (idletick2 > systemtickfrequency_impl)
-			strcpy(CPU_Usage2, "Core #2: 0.00%");
-		else snprintf(CPU_Usage2, sizeof CPU_Usage2, "Core #2: %.2f%%", (1.d - ((double)idletick2 / systemtickfrequency_impl)) * 100);
+			strcpy(CPU_Usage2, "UpdateDefaultCPUCore2FullOverlayCustomDrawerText"_tr.c_str());
+		else snprintf(CPU_Usage2, sizeof CPU_Usage2, "UpdateCPUCore2FullOverlayCustomDrawerText"_tr.c_str(), (1.d - ((double)idletick2 / systemtickfrequency_impl)) * 100);
 		if (idletick3 > systemtickfrequency_impl)
-			strcpy(CPU_Usage3, "Core #3: 0.00%");
-		else snprintf(CPU_Usage3, sizeof CPU_Usage3, "Core #3: %.2f%%", (1.d - ((double)idletick3 / systemtickfrequency_impl)) * 100);
+			strcpy(CPU_Usage3, "UpdateDefaultCPUCore3FullOverlayCustomDrawerText"_tr.c_str());
+		else snprintf(CPU_Usage3, sizeof CPU_Usage3, "UpdateCPUCore3FullOverlayCustomDrawerText"_tr.c_str(), (1.d - ((double)idletick3 / systemtickfrequency_impl)) * 100);
 
 		snprintf(CPU_compressed_c, sizeof CPU_compressed_c, "%s\n%s\n%s\n%s", CPU_Usage0, CPU_Usage1, CPU_Usage2, CPU_Usage3);
 
@@ -227,7 +226,7 @@ public:
 			int32_t deltaCPU = (int32_t)(realCPU_Hz / 1000) - (CPU_Hz / 1000);
 			snprintf(DeltaCPU_c, sizeof(DeltaCPU_c), "Δ %d.%u", deltaCPU / 1000, abs(deltaCPU / 100) % 10);
 		}
-		
+
 		///GPU
 		snprintf(GPU_Hz_c, sizeof GPU_Hz_c, "%u.%u MHz", GPU_Hz / 1000000, (GPU_Hz / 100000) % 10);
 		if (realGPU_Hz) {
@@ -235,8 +234,8 @@ public:
 			int32_t deltaGPU = (int32_t)(realGPU_Hz / 1000) - (GPU_Hz / 1000);
 			snprintf(DeltaGPU_c, sizeof(DeltaGPU_c), "Δ %d.%u", deltaGPU / 1000, abs(deltaGPU / 100) % 10);
 		}
-		snprintf(GPU_Load_c, sizeof GPU_Load_c, "Load: %u.%u%%", GPU_Load_u / 10, GPU_Load_u % 10);
-		
+		snprintf(GPU_Load_c, sizeof GPU_Load_c, "UpdateGPULoadFullOverlayCustomDrawerText"_tr.c_str(), GPU_Load_u / 10, GPU_Load_u % 10);
+
 		///RAM
 		snprintf(RAM_Hz_c, sizeof RAM_Hz_c, "%u.%u MHz", RAM_Hz / 1000000, (RAM_Hz / 100000) % 10);
 		if (realRAM_Hz) {
@@ -265,30 +264,30 @@ public:
 		snprintf(FULL_RAM_system_c, sizeof(FULL_RAM_system_c), "%4.2f / %4.2f MB", RAM_Used_system_f, RAM_Total_system_f);
 		snprintf(FULL_RAM_systemunsafe_c, sizeof(FULL_RAM_systemunsafe_c), "%4.2f / %4.2f MB", RAM_Used_systemunsafe_f, RAM_Total_systemunsafe_f);
 		snprintf(RAM_var_compressed_c, sizeof(RAM_var_compressed_c), "%s\n%s\n%s\n%s\n%s", FULL_RAM_all_c, FULL_RAM_application_c, FULL_RAM_applet_c, FULL_RAM_system_c, FULL_RAM_systemunsafe_c);
-		
+
 		if (R_SUCCEEDED(sysclkCheck)) {
 			int RAM_GPU_Load = ramLoad[SysClkRamLoad_All] - ramLoad[SysClkRamLoad_Cpu];
-			snprintf(RAM_load_c, sizeof RAM_load_c, 
-				"Load: %u.%u%% (CPU %u.%u | GPU %u.%u)",
+			snprintf(RAM_load_c, sizeof RAM_load_c,
+				"UpdateRAMLoadFullOverlayCustomDrawerText"_tr.c_str(),
 				ramLoad[SysClkRamLoad_All] / 10, ramLoad[SysClkRamLoad_All] % 10,
 				ramLoad[SysClkRamLoad_Cpu] / 10, ramLoad[SysClkRamLoad_Cpu] % 10,
 				RAM_GPU_Load / 10, RAM_GPU_Load % 10);
 		}
 		///Thermal
 		if (hosversionAtLeast(10,0,0)) {
-			snprintf(SoCPCB_temperature_c, sizeof SoCPCB_temperature_c, 
-				"%2.1f\u00B0C\n%2.1f\u00B0C\n%2d.%d\u00B0C", 
+			snprintf(SoCPCB_temperature_c, sizeof SoCPCB_temperature_c,
+				"%2.1f\u00B0C\n%2.1f\u00B0C\n%2d.%d\u00B0C",
 				SOC_temperatureF, PCB_temperatureF, skin_temperaturemiliC / 1000, (skin_temperaturemiliC / 100) % 10);
 		}
 		else {
-			snprintf(SoCPCB_temperature_c, sizeof SoCPCB_temperature_c, 
-				"%2d.%d\u00B0C\n%2d.%d\u00B0C\n%2d.%d\u00B0C", 
-				SOC_temperatureC / 1000, (SOC_temperatureC / 100) % 10, 
+			snprintf(SoCPCB_temperature_c, sizeof SoCPCB_temperature_c,
+				"%2d.%d\u00B0C\n%2d.%d\u00B0C\n%2d.%d\u00B0C",
+				SOC_temperatureC / 1000, (SOC_temperatureC / 100) % 10,
 				PCB_temperatureC / 1000, (PCB_temperatureC % 100) % 10,
 				skin_temperaturemiliC / 1000, (skin_temperaturemiliC / 100) % 10);
 		}
-		snprintf(Rotation_SpeedLevel_c, sizeof Rotation_SpeedLevel_c, "Fan Rotation Level: %2.1f%%", Rotation_SpeedLevel_f * 100);
-		
+		snprintf(Rotation_SpeedLevel_c, sizeof Rotation_SpeedLevel_c, "UpdateFanRotationLevelFullOverlayCustomDrawerText"_tr.c_str(), Rotation_SpeedLevel_f * 100);
+
 		///FPS
 		snprintf(FPS_var_compressed_c, sizeof FPS_var_compressed_c, "%u\n%2.1f", FPS, FPSavg);
 
@@ -301,11 +300,11 @@ public:
 			snprintf(remainingBatteryLife, sizeof remainingBatteryLife, "%d:%02d", batTimeEstimate / 60, batTimeEstimate % 60);
 		}
 		else snprintf(remainingBatteryLife, sizeof remainingBatteryLife, "-:--");
-		snprintf(BatteryDraw_c, sizeof BatteryDraw_c, "Battery Power Flow: %+.2fW[%s]", PowerConsumption, remainingBatteryLife);
+		snprintf(BatteryDraw_c, sizeof BatteryDraw_c, "UpdateBatteryPowerFlowFullOverlayCustomDrawerText"_tr.c_str(), PowerConsumption, remainingBatteryLife);
 		mutexUnlock(&mutex_BatteryChecker);
-		
 	}
-	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, touchPosition touchInput, JoystickPosition leftJoyStick, JoystickPosition rightJoyStick) override {
+
+	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, const HidTouchState &touchPos, HidAnalogStickState leftJoyStick, HidAnalogStickState rightJoyStick) override {
 		if (isKeyComboPressed(keysHeld, keysDown, mappedButtons)) {
 			TeslaFPS = 60;
 			tsl::goBack();
